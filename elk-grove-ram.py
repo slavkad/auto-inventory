611e50bc787a3fa8e57a36f2c479dcd226df68d4
slavka <a.b@c.com>
2021-08-28 14:00:04 -0700

    first checkin

---
+++ elk-grove-ram.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+# get JSON of list of vehicles matching the filter.
+# customized URL to paginate on 100 vehicles.
+url = 'https://www.elkgrovedodge.net/apis/widget/INVENTORY_LISTING_DEFAULT_AUTO_NEW:inventory-data-bus1/getInventory?make=Ram&bodyStyle=Crew%20Cab&driveLine=4x4&normalFuelType=Diesel&sortBy=internetPrice%20asc&pageSize=100'
+
+response = json.loads(requests.get(url).text)
+
+# loop throught he cars and pull the details out
+print('{:4s} | {:16s} | {:42s} | {:8s} | {:8s} | {:6s}  '.format("Year", "Model", "Trim", "MSRP", "Final", "% Off"))
+print ('-' * 100)
+for i in response["pageInfo"]['trackingData']:
+    modelYear = i['modelYear']
+    trim = i['trim']
+    model = i['model']
+    msrp = i['pricing']['msrp'].replace(',', '').replace('$', '')
+    final = i['pricing']['finalPrice'].replace(',', '').replace('$', '')
+    msrp2Print = i['pricing']['msrp']
+    final2Print = i['pricing']['finalPrice']
+    # calculate percent off from MSRP to final price and round to 2 decimal points
+    # negative number is discount
+    # positive number is markup
+    discountPercent =  (((int(final) / int(msrp)) -1) * 100)
+    discountPercent = round(discountPercent, 2)
+    print('{:4s} | {:16s} | {:42s} | {:8s} | {:8s} | {:6s}  '.format(str(modelYear), str(model), str(trim), str(msrp2Print), str(final2Print), str(discountPercent)))
